a764149fdbe3b005864f197ae19024c60480ac30
TheNetRunner <davidsmith@digitalsmithing.co.uk>
2018-05-07 13:31:50 +0100

    add comments to further explain code. created return_sum_of_multiples function to return the total of any number of lists

--- multiples_of_multiples.py
+++ multiples_of_multiples.py
@@ -10,9 +10,34 @@
 
 
 def multiples_of_multiples(start_num, end_num, multiple_num):
+    # init list of multiple items
     multiples_of_num = []
+    # loop through the range provided by the arguments
     for i in range(start_num, end_num):
+        # check to see if the number has a remainder of 0
         if i % multiple_num == 0:
+            # if the number is remainder 0 then add it to the list
             multiples_of_num.append(i)
+            # returns a list of multiple numbers
     return multiples_of_num
 
+# This function takes any number of lists as an argument
+# it then iterates through the lists and returns the sum total
+
+
+def return_sum_of_multiples(*args):
+    # init of sum total
+    sum_total = 0
+    print(args)
+    # loop over args which come in as tuples
+    for arg in args:
+        # loop over items in the lists inside the tuples
+        for i in arg:
+            # add each item in the lists to the sum total
+            sum_total += i
+    return sum_total
+
+
+a = return_sum_of_multiples(multiples_of_multiples(1, 1001, 3), multiples_of_multiples(1, 1001, 5))
+
+print("Your total is: %a" % a)
